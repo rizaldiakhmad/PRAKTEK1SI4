2501937e9735ce50a78fb39762ca9f2ac52e6248
RizaldiAkhmad <32384084+RizaldiAkhmad@users.noreply.github.com>
2018-03-22 21:21:00 +0800

    Membuat Atribut dan Behavior

--- src/praktek1si4/Buah.java
+++ src/praktek1si4/Buah.java
@@ -10,5 +10,17 @@ package praktek1si4;
  * @author rizaldi
  */
 public class Buah {
-    
+     String warna;
+     String rasa;
+     String bentuk;
+     int ukuran;
+     int masapanen;
+     
+     void cetakInfo(){
+     System.out.println("warna      :"+warna);
+     System.out.println("rasa       :"+rasa);
+     System.out.println("bentuk     :"+bentuk);
+     System.out.println("ukuran     :"+ukuran);
+     System.out.println("masa panen :"+masapanen);
+    }
 }
